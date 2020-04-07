0ee36d34945dc1276f7e690dbc1a1468eac7ccf3
github username <github email address>
2020-04-06 21:03:24 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,55 +1,33 @@
-require 'pry'
-
 class CashRegister
 
-  attr_accessor :discount, :total, :last_item
+  attr_accessor :total
+  attr_reader :discount
+  attr_reader :items
 
 
-  def initialize(discount = 0)
+  def initialize(discount=0)
     @total = 0
     @discount = discount
-    @cart = []
-    #@last_item = @cart[-1]
+    @items = []
   end
 
-  def add_item(title, price, qty = 1)
-    items = {}
-    items[:name] = items
-    items[:price] = price
-    items[:qty] = qty
-
-    @cart << items
-    @total = @total + price * qty
+  def add_item(item, price, qty=1)
+    qty.times { @items << item }
+    @total += price * qty
+    @last_transaction = price * qty
   end
 
   def apply_discount
-    if discount == 0
-      return "There is no discount to apply."
+    if @discount == 0
+     return "There is no discount to apply."
+    else
+      @total -= @total * @discount / 100
+      "After the discount, the total comes to $#{@total}."
     end
-
-    @total = @total - @total * discount / 100
-    return "After the discount, the total comes to $#{@total}."
-  end
-
-  def items
-    # item_list = []
-    # @cart.each { |item| item_list << items[:name] }
-    # item_list
-    # !!! getting infinite loop??
   end
 
   def void_last_transaction
-    # @cart = #delete last item from hash
-    # @total
-
-    if @cart == []
-      @total = 0.0
-    end
-
-    @total
-
+    @total -= @last_transaction
   end
-
-
 
 end
